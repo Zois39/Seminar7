bb28bd86059f1e4d4bbff2310c1f8896918c7b7e
konstantin bondarenko <zois1990@gmail.com>
2022-08-30 15:47:20 +0200

    first task complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//написать программу, которая создаст двумерный массив размером m * n, заполненный случайными числами
+
+int[,] RandomArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for(int i = 0; i < m; i++)
+        for(int j = 0; j < n; j++)
+            result[i, j] = new Random().Next(0, 100);
+    return result;
+}
+
+int Size(string str)
+{
+    int result;
+    Console.WriteLine($"Input size {str}");
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void ShowArray(int[,] array, int m, int n)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int m = Size("m");
+int n = Size("n");
+ShowArray(RandomArray(m, n), m, n);
